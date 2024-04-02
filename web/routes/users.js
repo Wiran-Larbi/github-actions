d0ec28598222dd16b5e3a272562a0f7eb483e6e3
DotCipher <71338128+Wiran-Larbi@users.noreply.github.com>
2024-04-02 15:14:15 +0000

    Update users.js
    adding post route for users

--- web/routes/users.js
+++ web/routes/users.js
@@ -6,4 +6,8 @@ router.get('/users', function (req, res, next) {
   res.send('respond with a resource')
 })
 
+router.post('/users', function (req, res, next) {
+  res.send('notify if added successfuly')
+})
+
 module.exports = router
